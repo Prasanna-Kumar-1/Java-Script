d17b029daf066b84f933eed8bb00c2f2c42ee742
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2021-03-08 21:01:37 +1100

    Implementing Array Split

---
+++ creatingrrayusingsplit.js
@@ -0,0 +1,11 @@
+// Creating an array using split
+
+let myName = "Prasanna";
+const myNameArray = myName.split("");
+
+console.log(myNameArray);
+
+let citiesAustralia = "Brisbane, Sydney, Melbourne, Adelide";
+const citiesAustraliaArray = citiesAustralia.split(",");
+
+console.log(citiesAustraliaArray);
