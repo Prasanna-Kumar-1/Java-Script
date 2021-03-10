8750bc86b7d6c1f82728603e64e40723e2acd140
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2021-03-10 19:29:50 +1100

    Array and Sort

---
+++ ArraySortAndReversal.js
@@ -0,0 +1,17 @@
+// Array Sort
+
+const ausCities = [
+  "Sydney",
+  "Darwin",
+  "Melbourne",
+  "Adelide",
+  "Perth",
+  "Brisbane",
+];
+
+ausCities.sort();
+console.log(ausCities);
+
+// Array Reversal
+ausCities.reverse();
+console.log(ausCities);
