b91356ed461441acde84a34aa2f2887c9e2d3542
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2021-03-17 18:54:49 +1100

    ShiftandUnshift

---
+++ shiftunshift.js
@@ -0,0 +1,15 @@
+//shift: Removing one array element in the beginning of the array.
+
+// push
+const arr = ["City1", "City2", "City3"];
+arr.shift();
+
+console.log(arr);
+
+//Add an element from the beginning
+//unshift: Adding array element in the beginning of the array.
+
+const arr1 = ["City1", "City2", "City3"];
+arr1.unshift("New City");
+
+console.log(arr1);
