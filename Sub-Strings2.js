b6790e3da05f0d43723683bcb6813eb3ba759053
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2021-03-21 17:50:45 +1100

    Sub-String

---
+++ Sub-Strings2.js
@@ -0,0 +1,16 @@
+// Sub-strings2
+
+// includes method
+
+let string1 = 'Prasanna loves JavaScript, it is Prasannas favorite';
+
+console.log(string1.includes('Prasanna')); //true
+
+// optional pos parameter. To start searching from that position
+
+console.log(string1.includes('Prasanna', 10));
+
+// starts with & ends with
+
+console.log(string1.startsWith('Pras'));
+console.log(string1.endsWith('rite'));
