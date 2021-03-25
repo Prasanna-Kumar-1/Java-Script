7685655fc0eda8767301445d173d6f604f4da873
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2021-03-25 19:38:32 +1100

    ClassChecking

---
+++ ClassChecking.js
@@ -0,0 +1,9 @@
+// // Class Checking : instance of
+
+// The instanceof operator allows to check whether an object belongs to a certain class. It also takes inheritance into account.
+
+class myClass {}
+let myObj = new myClass();
+
+console.log(myObj instanceof myClass);
+// Result : true
