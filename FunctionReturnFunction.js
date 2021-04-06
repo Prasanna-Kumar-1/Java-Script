5ce05dc293473f55e7e1f489e1f41395dd498681
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2021-04-06 18:04:09 +1000

    FunctionReturnFunction

---
+++ FunctionReturnFunction.js
@@ -0,0 +1,14 @@
+// function returning new function
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`Hello Mr.${name}! ${greeting}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+// function greet here retunns a new function and we store it in constant
+
+greeterHey('Prasanna');
+
+// using Arrow functions
