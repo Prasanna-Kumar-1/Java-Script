69042afe7f5754103f0652f1187d943246fdd27e
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2021-04-08 21:07:27 +1000

    Function Declaration

--- Function Declaration.js
+++ Function Declaration.js
@@ -1,41 +1,40 @@
 // Function Declaration
 function calcAge(birthYear) {
-    return 2040 - birthYear
+  return 2040 - birthYear;
 }
 
-const age1 = calcAge(1978)
+const age1 = calcAge(1978);
 
-console.log(age1)
+console.log(age1);
 
 // Difference between function declartion vs function expression is that
 // function declarion can be used before it is defined as shown below
 
-const age2 = calcAge(1978)
+const age2 = calcAge(1978);
 
 function calcAge(birthYear) {
-    return 2040 - birthYear
+  return 2040 - birthYear;
 }
 
-console.log(age2)
+console.log(age2);
 
 // function expression
- Here we write function keyword without a name. This is called Ananymus function
-
+//  Here we write function keyword without a name. This is called Ananymus function
 
 const calcAge3 = function (birthYear) {
-    return 2040 - birthYear
-}
+  return 2040 - birthYear;
+};
 
-const age3 = calcAge3(1978)
+const age3 = calcAge3(1978);
 
-console.log(age3)
+console.log(age3);
 
 // Function Expression cannot be accessed or used before it is defined as shown below
 
-const age4 = calcAge4(1978)
+const age4 = calcAge4(1978);
 
 const calcAge4 = function (birthYear) {
-    return 2040 - birthYear
-}
+  return 2040 - birthYear;
+};
 
-console.log(age4)
+console.log(age4);
