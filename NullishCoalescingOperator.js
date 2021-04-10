5a1ebcf66ca718761fd7a40aa69b88e8e2f55e04
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2021-04-10 22:42:15 +1000

    Nullish Coalescing Operator

---
+++ NullishCoalescingOperator.js
@@ -0,0 +1,11 @@
+// Nullish Coalescing Operator
+
+// ?? returns the first argument if it’s not null/undefined. Otherwise, the second one.
+
+let user1;
+
+console.log(user1 ?? 'Anonymous'); // Here user not defined, so it returns second one
+
+let user2 = 'Prasanna';
+
+console.log(user2 ?? 'Prasanna');
