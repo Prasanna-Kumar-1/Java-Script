e0cd4ea5304849ee555ed52f8ebb23a12a3dbccd
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2021-04-16 19:18:56 +1000

    Array Concepts

---
+++ Arrays2.js
@@ -0,0 +1,22 @@
+// Array Concepts
+
+const ausCities = ['Brisband', 'Sydney', 'Melbourne'];
+console.log(ausCities);
+
+const ausStates = new Array('NSW', 'VIC', 'SA');
+console.log(ausStates);
+
+// Accessing individual elements
+
+console.log(ausCities[1]);
+console.log(ausStates[0]);
+
+// Calculating length of the array
+
+console.log(ausCities.length);
+console.log(ausStates.length);
+
+//Mutation - Change
+
+ausCities[2] = 'Darwin';
+console.log(ausCities);
