f49a6af3d9c208283d1ec98bcdeb62064d1980df
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2022-02-27 21:53:25 +1100

    Updated Code

---
+++ Anagram.js
@@ -0,0 +1,10 @@
+// Two strings are anagrams if they are made of the same letters
+// arranged differently (ignoring the case).
+
+// Example : This is a string & Is this a string
+function checkAnagram(str1, str2) {
+    if(str1.)
+}
+
+
+checkAnagram("This is a string","Is this a string")
