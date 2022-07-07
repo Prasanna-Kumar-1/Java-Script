a314f028705057537a12d62d12c97d6551409fe3
Prasanna-Kumar-1 <prasanna.kumar@outlook.com.au>
2022-07-07 21:09:57 +1000

    ArraySlice

--- ArraySlice2.js
+++ ArraySlice2.js
@@ -2,10 +2,11 @@
 
 // arr.slice([start], [end])
 
-// It returns a new array copying to it all items from index start to end (not including end). Both start and end can be negative, in that case position from array end is assumed.
+// It returns a new array copying to it all items from index start to end (not including end).
+// Both start and end can be negative, in that case position from array end is assumed.
 
 // It’s similar to a string method str.slice, but instead of substrings it makes subarrays.
 
-let arr4 = ['Prasanna', 'is', 'excited', 'now'];
+let arr4 = ["Prasanna", "is", "excited", "now"];
 
 console.log(arr4.slice(0, 3));
